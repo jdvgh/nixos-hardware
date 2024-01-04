224556f144b77b4dec4f2c0b2cf1045b8f32fbab
Eilers, Jonas <133217951+jdvgh@users.noreply.github.com>
2024-01-04 09:22:05 +0100

    feat: Nvidia by default

--- lenovo/legion/16ach6h/default.nix
+++ lenovo/legion/16ach6h/default.nix
@@ -1,7 +1,7 @@
 { ... }:
 
 {
-  imports = [ ./hybrid ];
+  imports = [ ./nvidia ];
 
   # specialisation.ddg.configuration = {
   #   # This specialisation is for the case where "DDG" (Dual-Direct GFX, A hardware feature that can enable in bios) is enabled, since the amd igpu is blocked at hardware level and the built-in display is directly connected to the dgpu, we no longer need the amdgpu and prime configuration.
